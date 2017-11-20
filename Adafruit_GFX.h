23dbf5b0a01d63f9d47e70dee3d00a3df483829f
Anne LH <lhanne05@126.com>
2017-11-20 13:33:30 +0800

    lh1

--- Adafruit_GFX.h
+++ Adafruit_GFX.h
@@ -27,6 +27,7 @@ class Adafruit_GFX : public Print {
   virtual void writeFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
   virtual void writeFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
   virtual void writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color);
+  virtual void Pentagram(int16_t x0, int16_t y0, int16_t r, uint16_t color)
   virtual void endWrite(void);
 
   // CONTROL API
